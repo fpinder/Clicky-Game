eaca75d8cc906b5ed1cb8190492ad3a7dec4275a
Frank Pinder <fpinder@comcast.net>
2019-10-26 11:11:17 -0700

    Updated Navbar file

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 
 const Navbar = props => (
   <div className="navbar ">
-    <div>Clicky Game</div>
+    <div><i>Clicky Game</i></div>
     <div className={props.navMsgColor}>{props.navMessage}</div>
     <div>
       Score: {props.score} <span className="pipe">|</span> High Score: {props.highScore}
