8813fc54dd56c6c7cbd3f2835cc0665e353b84d1
Frank Pinder <fpinder@comcast.net>
2019-10-26 10:53:56 -0700

    updated

---
+++ src/images.js
@@ -0,0 +1,23 @@
+import akita from "./images/akita.jpg";
+import cairn from "./images/cairn.jpg";
+import hound from "./images/hound.jpg";
+import vizsla from "./images/vizsla.jpg";
+import whippet from "./images/whippet.jpg";
+import briard from "./images/briard.jpg";
+import chihuahua from "./images/chihuahua.jpg";
+import shiba from "./images/shiba.jpg";
+import waterdog from "./images/waterdog.jpg";
+
+const images = [
+  akita,
+  cairn,
+  hound,
+  vizsla,
+  whippet,
+  briard,
+  chihuahua,
+  shiba,
+  waterdog
+];
+
+export default images;
